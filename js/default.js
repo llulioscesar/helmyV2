f351d6f48fb84af84aeb3264bdb679c440a588ec
AndresSmith <soler8726@gmail.com>
2020-11-19 18:34:39 -0500

    0.0.1

--- js/default.js
+++ js/default.js
@@ -74,6 +74,7 @@ $(function () {
    $('#productsRe').on('click', function () {
       $("#back").removeClass("backWe");
       $("#back").addClass("backn");
+      $("#back").css("overflow-y", "scroll");
       $('#hom').css("display", "none");
       $('#log').css("display", "block");
       $('#line').css("display", "none");
--- products.php
+++ products.php
@@ -1,35 +1,63 @@
-<div class="contePro"></div>
-        <div class="hel">
-            <img src="css/images/GrupoFifty.png" alt="">
-        </div>
-        <div class="fityFour">
-            <h3>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                Bienvenido al mundo de Helmy<br>
-                ¡el mejor invento después de las motos!
-            </h3>
-            <h4>Helmy es un producto que consta de dos dispositivos. Ambos<br>
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;los vas a configurar y a usar desde la aplicación celular<br>
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Helmy-APP.
-            </h4>
-        </div>
-        <div class="textProducts">
-            <h3>FUNCIONALIDADES</h3>
-        </div>
-        <div class="all"></div>
-        <div class="lineProducts"></div>
-        <div class="funcionPro">
-            <img src="css/images/funcionPro.png" alt="">
-        </div>
-        <div class="lineTwoProducts"></div>
-        <div class="textOneProducts">
-            <h3>Este producto nació para proteger tú<br> 
-                vida y la de aquellos que más amas,<br>  
-                incluyendo tú motocicleta.
-            </h3>
-        </div>         
+<div class="conteScrollPro"></div>
+<div class="hel">
+    <img src="css/images/GrupoFifty.png" alt="">
+</div>
+<div class="fityFour">
+    <h3>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        Bienvenido al mundo de Helmy<br>
+        ¡el mejor invento después de las motos!
+    </h3>
+    <h4>Helmy es un producto que consta de dos dispositivos. Ambos<br>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;los vas a configurar y a usar desde la aplicación celular<br>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Helmy-APP.
+    </h4>
+</div>
+<div class="textProducts">
+    <h3>FUNCIONALIDADES</h3>
+</div>
+<div class="all"></div>
+<div class="lineProducts"></div>
+<div class="funcionPro">
+    <img src="css/images/funcionPro.png" alt="">
+</div>
+<div class="lineTwoProducts"></div>
+<div class="textOneProducts">
+    <h3>Este producto nació para proteger tú<br> 
+        vida y la de aquellos que más amas,<br>  
+        incluyendo tú motocicleta.
+    </h3>
+</div>    
+<div class="MPro">
+    <img src="css/images/M.png" alt="">
+</div>
+<div class="circleMPro">
+    <img src="css/images/BGCirclePro.png" alt="">
+</div>
+<div class="circleCPro">
+    <img src="css/images/BGCirclePro.png" alt="">
+</div>
+<div class="helmyMPro">
+    <img src="css/images/helmyMPro.png" alt="">
+</div>
+<div class="textCPro">
+    <h1>Helmy M</h1>
+    <p>Helmy-M se instala conectado<br>
+       directamente al encendido de tú<br> 
+       motocicleta y solo lo puede<br>
+       hacer personal calificado.
+    </p>
+    <h5>Este producto nació para<br>proteger tú activo más valioso.<br>Tú motocicleta.</h5>
+    <a href="javascript:void(0)" id="BtnMProducto">VER MÁS</a>
+</div>
+<div class="CPro">
+    <img src="css/images/CPro.png" alt="">
+</div>
+<div class="helmyCPro">
+    <img src="css/images/helmyCPro.png" alt="">
+</div>
 <div class="naverProducts">
     <div class="menu">
         <li><img src="css/images/BotonMenu.png" alt="Click"></li>
