edfe4aee93d7387928b650de4d65d39e6865708c
AndresSmith <soler8726@gmail.com>
2020-11-19 11:09:02 -0500

    0.0.1

--- funciones/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define("GOOGLE", [
-    'clientId'     => '501584891263-brd7hqrg9auv8b95nurusomgg6lkhehi.apps.googleusercontent.com',
-    'clientSecret' => '570tMc9D2WrVOb4XQsm4ISf3',
-    'redirectUri'  => 'http://localhost/project/inicio'
-]);
-
-?>
