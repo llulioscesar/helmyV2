7136151ad630bf66f858bc65f2a7b059bd48c057
Julio César <llulioscesar@gmail.com>
2020-12-11 11:49:03 -0500

    Update db.php

--- functions/db.php
+++ functions/db.php
@@ -5,7 +5,7 @@ class db {
     public function __construct(){
 
         try {
-            $this->con = new PDO("mysql:host=" . 'localhost' . ";dbname=" . 'helmy' , 'root' , '', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+            $this->con = new PDO("mysql:host=" . 'klbcedmmqp7w17ik.cbetxkdyhwsb.us-east-1.rds.amazonaws.com' . ";dbname=" . 'znngwbkiqcar8e2z' , 'lgt795lv3xdgv7o0' , 'nb9jtgm9nr4yrbep', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
         }catch(PDOException $e) {
             echo "Connection Error: " . $e->getMessage();
         }
