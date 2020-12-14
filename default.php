47f250a2d22478450c14659b960f0c7ed5600ce4
AndresSmith <soler8726@gmail.com>
2020-12-14 16:48:04 -0500

    0.0.1

--- default.php
+++ default.php
@@ -8,6 +8,14 @@
     <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/styles.css">
     <link rel="icon" href="css/images/favicon.png">
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-181478155-1"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-181478155-1');
+    </script>
     <script src="https://apis.google.com/js/api:client.js" async defer></script>
     <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDs96e8EtyL3HL_V_RffZlkcMZVXbLG3Zs&libraries=places"></script>
